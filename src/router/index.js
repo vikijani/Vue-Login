f4fe27655284fba758036571239ad15f2b10bf1d
uncleVik <vkoohjani00@gmail.com>
2026-07-07 10:44:08 +0330

    add pages and setup router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
+import DashboardPage from '@/DashboardPage.vue'
+import LoginPage from '@/LoginPage.vue'
+import SignInPage from '@/SignInPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {path: "/", component: LoginPage},
+  {path: "/signin", component: SignInPage},
+  {path: "/dashboard", component: DashboardPage},
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: routes,
 })
 
 export default router
